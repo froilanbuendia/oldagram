c256f22084cbdbde0bc359bb5a24ec27bb9078f1
froilanbuendia <froilangbuendia@gmail.com>
2025-07-01 21:58:53 -0700

    requirements and stretch goals template

--- index.js
+++ index.js
@@ -42,3 +42,49 @@ userLocationVangogh.textContent = posts[0].location;
 postImageVangogh.src = posts[0].post;
 likesVangogh.textContent = `${posts[0].likes} likes`;
 usernameVangogh.innerHTML = `${posts[0].username} <span class='comment-text'>${posts[0].comment}</span>`;
+
+const avatarCourbet = document.getElementById("user-avatar-courbet");
+const nameCourbet = document.getElementById("name-courbet");
+const userLocationCourbet = document.getElementById("user-location-courbet");
+const postImageCourbet = document.getElementById("post-image-courbet");
+const likesCourbet = document.getElementById("post-likes-courbet");
+const usernameCourbet = document.getElementById("user-name-courbet");
+
+avatarCourbet.src = posts[1].avatar;
+nameCourbet.textContent = posts[1].name;
+userLocationCourbet.textContent = posts[1].location;
+postImageCourbet.src = posts[1].post;
+likesCourbet.textContent = `${posts[1].likes} likes`;
+usernameCourbet.innerHTML = `${posts[1].username} <span class='comment-text'>${posts[1].comment}</span>`;
+
+const avatarDucreux = document.getElementById("user-avatar-ducreux");
+const nameDucreux = document.getElementById("name-ducreux");
+const userLocationDucreux = document.getElementById("user-location-ducreux");
+const postImageDucreux = document.getElementById("post-image-ducreux");
+const likesDucreux = document.getElementById("post-likes-ducreux");
+const usernameDucreux = document.getElementById("user-name-ducreux");
+
+avatarDucreux.src = posts[2].avatar;
+nameDucreux.textContent = posts[2].name;
+userLocationDucreux.textContent = posts[2].location;
+postImageDucreux.src = posts[2].post;
+likesDucreux.textContent = `${posts[2].likes} likes`;
+usernameDucreux.innerHTML = `${posts[2].username} <span class='comment-text'>${posts[2].comment}</span>`;
+
+const likedVangogh = document.getElementById("liked-vangogh");
+likedVangogh.addEventListener("dblclick", () => {
+  posts[0].likes += 1;
+  likesVangogh.textContent = `${posts[0].likes} likes`;
+});
+
+const likedCourbet = document.getElementById("liked-courbet");
+likedCourbet.addEventListener("dblclick", () => {
+  posts[1].likes += 1;
+  likesCourbet.textContent = `${posts[1].likes} likes`;
+});
+
+const likedDucreux = document.getElementById("liked-ducreux");
+likedDucreux.addEventListener("dblclick", () => {
+  posts[2].likes += 1;
+  likesDucreux.textContent = `${posts[2].likes} likes`;
+});
